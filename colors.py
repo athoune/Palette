97138a86b6526201968415f5979b6dbfb6b1a37b
Mathieu Lecarme <mathieu@garambrogne.net>
2009-07-07 19:29:24 +0200

    refactoring

--- colors.py
+++ colors.py
@@ -2,15 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import colorsys
+import Image
 
 def rgb2hsv(rgb):
 	"rgb to hsv conversion with value between 0 and 255"
-	tmp = colorsys.rgb_to_hsv(rgb[0]/256.0,rgb[1]/256.0,rgb[2]/256.0)
+	tmp = colorsys.rgb_to_hsv(rgb[0]/255.0,rgb[1]/255.0,rgb[2]/255.0)
 	return (int(tmp[0]*255),int(tmp[1]*255),int(tmp[2]*255))
 
-class Matrix:
-	"a 256 x 256 matrix, hue, saturation, with frequency as value"
-	def __init__(self, image):
+def hsv2rgb(hsv):
+	"hsv to rgb conversion with value between 0 and 255"
+	tmp = colorsys.hsv_to_rgb(hsv[0]/256.0,hsv[1]/256.0,hsv[2]/256.0)
+	return (int(tmp[0]*255),int(tmp[1]*255),int(tmp[2]*255))
+
+class ColorMap(object):
+	"""a 256 x 256 matrix, hue, saturation, with frequency as value
+	name: name of the file
+	max: is the max value
+	data: is the matrix
+	"""
+	def __init__(self, path):
+		image = Image.open(path)
+		self.name = '.'.join(path.split('/')[-1].split('.')[:-1])
 		self.data = {}
 		for h in range(256):
 			self.data[h] = {}
@@ -24,11 +36,11 @@ class Matrix:
 				h,s,v = rgb2hsv(point)
 				self.data[h][s] += 1
 				self.max = max(self.data[h][s], self.max)
+	def __getitem__(self, key):
+		return self.data[key]
 
 if __name__ == "__main__":
-	import Image
 	import sys
-	image = Image.open(sys.argv[1])
-	matrix = Matrix(image)
-	print matrix.max
+	colormap = ColorMap(sys.argv[1])
+	print colormap.name, colormap.max
 	#print matrix.data
