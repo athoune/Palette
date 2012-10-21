b41d786a306a51f728628709f82dea25ead8251d
Mathieu Lecarme <mlecarme@bearstech.com>
2012-10-21 22:18:02 +0200

    color map as numpy array

--- map.py
+++ map.py
@@ -2,18 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
+from numpy import array
 
 
-def colormap(path):
+def colormap(path, size=8):
     cmd = ['convert', path, '-colorspace', 'Lab', '-format', '%c', 'histogram:info:-']
     # convert 51963fc1422ea82f6df3deefcea66b9ee7892630_m.jpg -colorspace LAB -format %c histogram:info:-
     pipe = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    m = array(size * [size * [0]])
+    d = 256 / size
+    t = 0.0
     for line in pipe.stdout.readlines():
         ll = line.strip().split(' ')
         if len(ll) > 1:
             n = int(ll[0][:-1])
+            t += n
             color = [int(i) for i in ll[-1][4:-1].split(',')]
-            print n, color
+            l, a, b = color
+            m[b / d][a / d] += n
+    print m * (256 / t)
+
 
 if __name__ == "__main__":
     import sys
