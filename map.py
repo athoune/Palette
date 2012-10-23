2d0ccc9dab85504c8c183825295e3eea39bf99a4
Mathieu Lecarme <mlecarme@bearstech.com>
2012-10-23 10:20:09 +0200

    Black and white

--- map.py
+++ map.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-from numpy import array
+from numpy import zeros
 
 
 def colormap(path, size=8):
-    cmd = ['convert', path, '-colorspace', 'Lab', '-format', '%c', 'histogram:info:-']
+    cmd = ['convert', path, '-colorspace', 'sRGB', '-colorspace', 'Lab', '-format', '%c', 'histogram:info:-']
     # convert 51963fc1422ea82f6df3deefcea66b9ee7892630_m.jpg -colorspace LAB -format %c histogram:info:-
     pipe = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    m = array(size * [size * [0]])
+    m = zeros([size, size], int)
     d = 256 / size
-    t = 0.0
+    t = 0
+    black = 0
+    white = 0
     for line in pipe.stdout.readlines():
         ll = line.strip().split(' ')
         if len(ll) > 1:
@@ -19,10 +21,25 @@ def colormap(path, size=8):
             t += n
             color = [int(i) for i in ll[-1][4:-1].split(',')]
             l, a, b = color
-            m[b / d][a / d] += n
-    print m * (256 / t)
+            if l < 5:
+                black += n
+            elif l > 250:
+                white += n
+            else:
+                #print b / d, a / d, b, a
+                m[b / d][a / d] += n
+    coeff = 256 * 256
+    return m * coeff / t, black * coeff / t, white * coeff / t
 
 
 if __name__ == "__main__":
     import sys
-    colormap(sys.argv[1])
+    if len(sys.argv) == 2:
+        m, b, w = colormap(sys.argv[1])
+        print m
+        print b, w
+    elif len(sys.argv) == 3:
+        m1, b1, w1 = colormap(sys.argv[1])
+        m2, b2, w2 = colormap(sys.argv[2])
+        m = m1 - m2
+        print abs(m)
