2c1edfe90e9ba2298c95ed8b15b3bc79ef3a5a5b
Mathieu Lecarme <mlecarme@bearstech.com>
2012-10-28 14:07:12 +0100

    Only new pictures.

--- ffffound.py
+++ ffffound.py
@@ -19,10 +19,10 @@ class ffffound(object):
 
 if __name__ == "__main__":
     for entry in ffffound():
-        print entry.title
         path = entry.media_content[0]['url']
         d = urllib2.urlopen(path)
         name = path.split('/')[-1]
         if not os.path.exists(name):
+            print entry.title
             with open(name, 'w') as l:
                 shutil.copyfileobj(d, l)
